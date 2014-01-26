838ec06a4e9beb3d132cbf06c92de4b3525eefd8
shanghaikid <jiangruiyi@gmail.com>
2014-01-26 16:56:39 +0800

    test

--- cbr/js/jquery.support.borderRadius.js
+++
@@ -1,1 +0,0 @@
-jQuery(function(){jQuery.support.borderRadius=false;jQuery.each(["borderRadius","BorderRadius","MozBorderRadius","WebkitBorderRadius","OBorderRadius","KhtmlBorderRadius"],function(){if(document.body.style[this]!==undefined){jQuery.support.borderRadius=true}return(!jQuery.support.borderRadius)})});
