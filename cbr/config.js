f3f93975148abd2c58fa3b7610a35f19fc458acd
shanghaikid <jiangruiyi@gmail.com>
2014-01-26 16:22:57 +0800

    totally clean

---
+++ cbr/config.js
@@ -0,0 +1,10 @@
+var dojoConfig = {
+        isDebug: true,
+        tlmSiblingOfDojo: false,
+        baseUrl: './js/',
+        packages: [
+            { name: "dojo", location: "lib/dojo" },
+            { name: "dijit", location: "lib/dijit"},
+            { name: "src", location: "src"}
+        ]
+ };
